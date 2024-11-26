5dd73c6e84feee60eb5d080d1eaa4e3147fa1b21
NguyenTrungDuy <duyntph49915@gmail.com>
2024-11-26 23:39:05 +0700

    update login, signup

--- app/src/main/java/com/scan/dyong_resident/LoginActivity.java
+++ app/src/main/java/com/scan/dyong_resident/LoginActivity.java
@@ -3,19 +3,28 @@ package com.scan.dyong_resident;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
     TextView tvLogin, tvDK;
-    EditText edtPhone, edtPass;
+    EditText edtEmail, edtPass;
     CheckBox cbSave;
     String userRegister = "", passRegister = "";
+    private TextInputEditText passwordEditText;
+    private TextInputLayout passwordTextInputLayout;
+    private boolean isPasswordVisible = false;
+    ImageView passwordToggleIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,39 +38,29 @@ public class LoginActivity extends AppCompatActivity {
         if(isRemember){
             String phone = sharedPreferences.getString("phone","");
             String pass = sharedPreferences.getString("pass","");
-            edtPhone.setText(phone);
-            edtPass.setText(pass);
+            edtEmail.setText(phone);
+//            edtPass.setText(pass);
             cbSave.setChecked(isRemember);
 
             userRegister = phone;
-            passRegister = pass;
+//            passRegister = pass;
         }
 
         tvLogin.setOnClickListener(v -> {
-            String phone = edtPhone.getText().toString();
-            String pass = edtPass.getText().toString();
+            String email = edtEmail.getText().toString();
+//            String pass = edtPass.getText().toString();
 
-            if (phone.length()>0 && pass.length()>0 && phone.equals(userRegister) && pass.equals(passRegister)){
-                if (cbSave.isChecked()){
-                    SharedPreferences preferences = getSharedPreferences("data",MODE_PRIVATE);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.putBoolean("isRemember",true);
-                    editor.putString("phone",phone);
-                    editor.putString("pass",pass);
-                    editor.apply();
-
-                }
-                else {
-                    SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.clear();
-                    editor.apply();
-                }
-
+            if (isValidEmail(email)){
                 Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(LoginActivity.this, BottomAppBarActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
+
+                SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.apply();
+
             }
             else {
                 Toast.makeText(this,"Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
@@ -74,13 +73,20 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+    private boolean isValidEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
     public void init(){
         tvLogin = findViewById(R.id.tvlogin);
         tvDK = findViewById(R.id.tvDK);
-        edtPhone = findViewById(R.id.edtPhone);
-        edtPass = findViewById(R.id.edtPass);
+        edtEmail = findViewById(R.id.edtMail);
+//        edtPass = findViewById(R.id.edtPass);
         cbSave = findViewById(R.id.cbSave);
-
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordTextInputLayout = findViewById(R.id.passwordTextInputLayout);
+        passwordTextInputLayout.setPasswordVisibilityToggleEnabled(true);
     }
 
 }
--- app/src/main/java/com/scan/dyong_resident/SignupActivity.java
+++ app/src/main/java/com/scan/dyong_resident/SignupActivity.java
@@ -13,7 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
     TextView tvSignup, tvDN;
-    EditText edtFullName, edtPhone, edtMail, edtPass, edtRePass;
+    EditText edtFullName, edtMail, edtPass, edtRePass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +42,8 @@ public class SignupActivity extends AppCompatActivity {
 
             }
         });
+
+
 
         edtMail.addTextChangedListener(new TextWatcher() {
             @Override
@@ -111,25 +113,19 @@ public class SignupActivity extends AppCompatActivity {
 
         tvSignup.setOnClickListener(v -> {
             String fullName = edtFullName.getText().toString();
-            String phone = edtPhone.getText().toString();
             String mail = edtMail.getText().toString();
             String pass = edtPass.getText().toString();
             String rePass = edtRePass.getText().toString();
 
             //validate
-            if(fullName.equals("") || phone.equals("") || mail.equals("") || pass.equals("") || rePass.equals("")){
+            if(fullName.equals("")  || mail.equals("") || pass.equals("") || rePass.equals("")){
                 if (fullName.equals("")){
                     edtFullName.setError("Vui lòng nhập họ tên");
                 }
                 else {
                     edtFullName.setError(null);
                 }
-                if (phone.equals("")){
-                    edtPhone.setError("Vui lòng nhập số điện thoại");
-                }
-                else {
-                    edtPhone.setError(null);
-                }
+
                 if (mail.equals("")){
                     edtMail.setError("Vui lòng nhập email");
                 }
@@ -155,7 +151,6 @@ public class SignupActivity extends AppCompatActivity {
             else {
                 Intent intent = new Intent();
                 intent.putExtra("fullName", fullName);
-                intent.putExtra("phone", phone);
                 intent.putExtra("mail", mail);
                 intent.putExtra("pass", pass);
                 setResult(1,intent);
@@ -175,7 +170,6 @@ public class SignupActivity extends AppCompatActivity {
         tvSignup = findViewById(R.id.tvSignup);
         tvDN = findViewById(R.id.tvDN);
         edtFullName = findViewById(R.id.edtFullName);
-        edtPhone = findViewById(R.id.edtPhone);
         edtMail = findViewById(R.id.edtMail);
         edtPass = findViewById(R.id.edtPass);
         edtRePass = findViewById(R.id.edtRePass);
