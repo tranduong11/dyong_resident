2df9ebda1771431d00ee69a2624aaf438bc9734f
NguyenTrungDuy <duyntph49915@gmail.com>
2024-11-26 08:14:54 +0700

    update login, signup, img_app

--- app/src/main/java/com/scan/dyong_resident/LoginActivity.java
+++ app/src/main/java/com/scan/dyong_resident/LoginActivity.java
@@ -1,16 +1,33 @@
 package com.scan.dyong_resident;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView tvLogin, tvDK;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_screen);
+        init();
+        tvLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, BottomAppBarActivity.class);
+            startActivity(intent);
+        });
+        tvDK.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+
     }
+    public void init(){
+        tvLogin = findViewById(R.id.tvlogin);
+        tvDK = findViewById(R.id.tvDK);
+    }
+
 }
--- app/src/main/java/com/scan/dyong_resident/SignupActivity.java
+++ app/src/main/java/com/scan/dyong_resident/SignupActivity.java
@@ -1,16 +1,32 @@
 package com.scan.dyong_resident;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
-
+    TextView tvSigup, tvDN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.signup_screen);
+        init();
+        tvSigup.setOnClickListener(v -> {
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+        tvDN.setOnClickListener(v -> {
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+    }
+    public void init(){
+        tvSigup = findViewById(R.id.tvSigup);
+        tvDN = findViewById(R.id.tvDN);
     }
 }
